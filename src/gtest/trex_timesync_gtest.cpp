fe5f0fdce415947739128cc614e5ed464787ec87
Mateusz Neumann <mateumann@gmail.com>
2019-11-29 16:02:42 +0100

    Test clock/port identification validation

--- src/gtest/trex_timesync_gtest.cpp
+++ src/gtest/trex_timesync_gtest.cpp
@@ -25,9 +25,13 @@ limitations under the License.
 
 #include "trex_timesync.h"
 
+#define MAX_SEC 2147483647
 #define PORT_ID 0
 #define SEQUENCE_ID 1
-#define MAX_SEC 2147483647
+#define CLOCK_ID_M1 {0x01, 0}
+#define CLOCK_ID_M2 {0x02, 0}
+#define CLOCK_ID_S1 {0x11, 0}
+#define CLOCK_ID_S2 {0x12, 0}
 
 class timesync_engine_test : public trexStlTest {
   protected:
@@ -119,4 +123,79 @@ TEST_F(timesync_engine_test, slave_delta_skip_deprecated_process) {
     EXPECT_EQ(-500 * 1000 * 1000, engine.getDelta(PORT_ID));
     engine.receivedPTPDelayResp(PORT_ID, SEQUENCE_ID, {5, 0}, {}, {});
     EXPECT_EQ(-500 * 1000 * 1000, engine.getDelta(PORT_ID));
+}
+
+TEST_F(timesync_engine_test, slave_delta_valid_src_port_id) {
+    CTimesyncEngine engine;
+    engine.setTimesyncMethod(TimesyncMethod::PTP);
+    engine.setTimesyncMaster(false);
+    engine.receivedPTPSync(PORT_ID, SEQUENCE_ID, {2, 0}, CLOCK_ID_M1);
+    engine.receivedPTPFollowUp(PORT_ID, SEQUENCE_ID, {1, 0}, CLOCK_ID_M1);
+    engine.sentPTPDelayReq(PORT_ID, SEQUENCE_ID, {3, 0}, CLOCK_ID_S1);
+    engine.receivedPTPDelayResp(PORT_ID, SEQUENCE_ID, {5, 0}, CLOCK_ID_M1, CLOCK_ID_S1);
+    EXPECT_EQ(500 * 1000 * 1000, engine.getDelta(PORT_ID));
+}
+
+TEST_F(timesync_engine_test, slave_delta_invalid_src_port_id_1) {
+    CTimesyncEngine engine;
+    engine.setTimesyncMethod(TimesyncMethod::PTP);
+    engine.setTimesyncMaster(false);
+    engine.receivedPTPSync(PORT_ID, SEQUENCE_ID, {2, 0}, CLOCK_ID_M1);
+    engine.receivedPTPFollowUp(PORT_ID, SEQUENCE_ID, {1, 0}, CLOCK_ID_M2);
+    engine.sentPTPDelayReq(PORT_ID, SEQUENCE_ID, {3, 0}, CLOCK_ID_S1);
+    engine.receivedPTPDelayResp(PORT_ID, SEQUENCE_ID, {5, 0}, CLOCK_ID_M1, CLOCK_ID_S1);
+    EXPECT_EQ(0 * 1000 * 1000, engine.getDelta(PORT_ID));
+}
+
+TEST_F(timesync_engine_test, slave_delta_invalid_src_port_id_2) {
+    CTimesyncEngine engine;
+    engine.setTimesyncMethod(TimesyncMethod::PTP);
+    engine.setTimesyncMaster(false);
+    engine.receivedPTPSync(PORT_ID, SEQUENCE_ID, {2, 0}, CLOCK_ID_M1);
+    engine.receivedPTPFollowUp(PORT_ID, SEQUENCE_ID, {1, 0}, CLOCK_ID_M1);
+    engine.sentPTPDelayReq(PORT_ID, SEQUENCE_ID, {3, 0}, CLOCK_ID_S1);
+    engine.receivedPTPDelayResp(PORT_ID, SEQUENCE_ID, {5, 0}, CLOCK_ID_M2, CLOCK_ID_S1);
+    EXPECT_EQ(0 * 1000 * 1000, engine.getDelta(PORT_ID));
+}
+
+TEST_F(timesync_engine_test, slave_delta_invalid_src_port_id_3) {
+    CTimesyncEngine engine;
+    engine.setTimesyncMethod(TimesyncMethod::PTP);
+    engine.setTimesyncMaster(false);
+    engine.receivedPTPSync(PORT_ID, SEQUENCE_ID, {2, 0}, CLOCK_ID_M1);
+    engine.receivedPTPFollowUp(PORT_ID, SEQUENCE_ID, {1, 0}, CLOCK_ID_M2);
+    engine.sentPTPDelayReq(PORT_ID, SEQUENCE_ID, {3, 0}, CLOCK_ID_S1);
+    engine.receivedPTPDelayResp(PORT_ID, SEQUENCE_ID, {5, 0}, CLOCK_ID_M2, CLOCK_ID_S1);
+    EXPECT_EQ(0 * 1000 * 1000, engine.getDelta(PORT_ID));
+}
+
+TEST_F(timesync_engine_test, slave_delta_mixed_src_port_id) {
+    CTimesyncEngine engine;
+    engine.setTimesyncMethod(TimesyncMethod::PTP);
+    engine.setTimesyncMaster(false);
+    engine.receivedPTPSync(PORT_ID, SEQUENCE_ID, {2, 0}, CLOCK_ID_M1);
+    engine.receivedPTPFollowUp(PORT_ID, SEQUENCE_ID, {1, 0}, CLOCK_ID_M1);
+    engine.sentPTPDelayReq(PORT_ID, SEQUENCE_ID, {3, 0}, CLOCK_ID_S1);
+    engine.receivedPTPDelayResp(PORT_ID, SEQUENCE_ID, {5, 0}, CLOCK_ID_M1, CLOCK_ID_S1);
+    EXPECT_EQ(500 * 1000 * 1000, engine.getDelta(PORT_ID));
+
+    engine.receivedPTPSync(PORT_ID, SEQUENCE_ID, {12, 0}, CLOCK_ID_M1);
+    engine.receivedPTPFollowUp(PORT_ID, SEQUENCE_ID, {11, 0}, CLOCK_ID_M1);
+    engine.sentPTPDelayReq(PORT_ID, SEQUENCE_ID, {13, 0}, CLOCK_ID_S1);
+    engine.receivedPTPDelayResp(PORT_ID, SEQUENCE_ID, {15, 0}, CLOCK_ID_M2, CLOCK_ID_S1);
+    EXPECT_EQ(500 * 1000 * 1000, engine.getDelta(PORT_ID));
+
+    engine.receivedPTPDelayResp(PORT_ID, SEQUENCE_ID, {16, 0}, CLOCK_ID_M1, CLOCK_ID_S1);
+    EXPECT_EQ(1000 * 1000 * 1000, engine.getDelta(PORT_ID));
+}
+
+TEST_F(timesync_engine_test, slave_delta_invalid_req_src_port_id) {
+    CTimesyncEngine engine;
+    engine.setTimesyncMethod(TimesyncMethod::PTP);
+    engine.setTimesyncMaster(false);
+    engine.receivedPTPSync(PORT_ID, SEQUENCE_ID, {2, 0}, CLOCK_ID_M1);
+    engine.receivedPTPFollowUp(PORT_ID, SEQUENCE_ID, {1, 0}, CLOCK_ID_M1);
+    engine.sentPTPDelayReq(PORT_ID, SEQUENCE_ID, {3, 0}, CLOCK_ID_S1);
+    engine.receivedPTPDelayResp(PORT_ID, SEQUENCE_ID, {5, 0}, CLOCK_ID_M1, CLOCK_ID_S2);
+    EXPECT_EQ(0 * 1000 * 1000, engine.getDelta(PORT_ID));
 }
