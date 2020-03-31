5165417f5c203fe112fdb85ab114c81c83f48de1
Artur Jaworski <github@mehthehedgehog.be>
2020-03-31 13:39:47 +0200

    revert changes

--- src/dpdk/lib/librte_eal/common/include/rte_time.h
+++ src/dpdk/lib/librte_eal/common/include/rte_time.h
@@ -39,12 +39,10 @@ rte_cyclecounter_cycles_to_ns(struct rte_timecounter *tc, uint64_t cycles)
 
 	/* Add fractional nanoseconds. */
 	ns = cycles + tc->nsec_frac;
-	ns = ns * 8;
-	tc->nsec_frac = ns % 10000000;
+	tc->nsec_frac = ns & tc->nsec_mask;
 
 	/* Shift to get only nanoseconds. */
-	//return ns >> tc->cc_shift;
-	return (ns / 10000000);
+	return ns >> tc->cc_shift;
 }
 
 /**
