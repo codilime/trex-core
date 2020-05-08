ba66a851f2d6d5ed9dd6bcb0f7804cb4ad3c52aa
Artur Jaworski <github@mehthehedgehog.be>
2020-05-08 20:45:16 +0200

    sdf

--- src/platform_cfg.cpp
+++ src/platform_cfg.cpp
@@ -341,15 +341,15 @@ void operator >> (const YAML::Node& node, CMacYamlInfo & mac_info) {
         mac_info.m_mpls_count = 0;
         for(YAML::Iterator it = mpls.begin(); it != mpls.end(); ++it) {
             std::string str_label;
-            uint32_t label;
-            uint32_t ethtype;
+            uint32_t label = 0;
+            uint32_t ethtype = 0;
 
             it.first() >> str_label;
-            it.second() >> ethtype;
+            //it.second() >> ethtype;
             if(str_label == "default"){
                 mac_info.m_mpls_def_type = ethtype;
             } else {
-                it.first() >> label;
+            //    it.first() >> label;
                 
                 mac_info.m_mpls_ethtype[mac_info.m_mpls_count] = {label, (uint16_t)ethtype};
                 mac_info.m_mpls_count += 1;
